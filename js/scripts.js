8d6b5e2c7a842fe451675a1fabe9ec986c24365e
pier <pierknows@gmail.com>
2023-06-20 00:58:31 -0400

    completed ui logic and fixed business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 //business logic 
-
 function beepBoop (n) {
+    const user = inputUser();
     let numbers = [];
-}
+
 for (let i = 0; i <= n; i++) {
     if (i.toString().includes(3)) {
         let i = "Won't you be my neighbor?";
@@ -15,10 +15,38 @@ for (let i = 0; i <= n; i++) {
     else if (i.toString().includes(1)){
         let i = "Beep";
         numbers.push(i);
-    } 
-    else {
+    } else { 
         numbers.push(i)
     };
     }
     return numbers;
+}
+
+function inputUser(){
+    const user = document.getElementById("name-Input").value;
+    return user;
+}
+
+//ui logic 
+
+window.addEventListener("load", function(){
+    document.getElementById("user-Input").addEventListener("submit", submitForm);
+
+});
+
+function submitForm(event){
+    event.preventDefault();
+    const user = document.getElementById("name-Input").value;
+    let inputNumber = document.getElementById("number-Input").value;
+    let h4 = document.getElementById("numbersArray");
+    let numbersArr= beepBoop(inputNumber);
+
+    if( user && inputNumber) {
+        h4.innerText = numbersArr.join(",");
+    }
+};
+const btn = document.querySelector("#refresh");
+    btn.addEventListener("click", function(){
+     location.reload();   
+});
 
